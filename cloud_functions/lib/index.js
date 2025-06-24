0711e4b88d51794fb6964187d1cb5349cf51e57a
Muzsy <150178846+Muzsy@users.noreply.github.com>
2025-06-24 06:09:52 +0200

    firebase fájlok módosítása

---
+++ cloud_functions/lib/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.coin_trx = void 0;
+var coin_trx_logic_1 = require("./coin_trx.logic");
+Object.defineProperty(exports, "coin_trx", { enumerable: true, get: function () { return coin_trx_logic_1.coin_trx; } });
