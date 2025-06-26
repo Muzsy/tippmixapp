d104b447cfd0ba2fc251fb88886e5ef54e49df8b
Muzsy <150178846+Muzsy@users.noreply.github.com>
2025-06-27 00:06:22 +0200

    coin service callable vászon+yaml

---
+++ cloud_functions/lib/daily_bonus.js
@@ -0,0 +1,66 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.daily_bonus = void 0;
+const functions = __importStar(require("firebase-functions"));
+const admin = __importStar(require("firebase-admin"));
+admin.initializeApp();
+const db = admin.firestore();
+exports.daily_bonus = functions
+    .region('europe-central2')
+    .pubsub.schedule('5 0 * * *')
+    .timeZone('Europe/Budapest')
+    .onRun(async () => {
+    const usersSnap = await db.collection('users').get();
+    const batch = db.batch();
+    usersSnap.docs.forEach((doc) => {
+        const userId = doc.id;
+        const logRef = db
+            .collection('users')
+            .doc(userId)
+            .collection('coin_logs')
+            .doc();
+        batch.set(logRef, {
+            userId,
+            amount: 50,
+            type: 'credit',
+            reason: 'daily_bonus',
+            transactionId: logRef.id,
+            timestamp: admin.firestore.FieldValue.serverTimestamp(),
+            description: 'Daily bonus',
+        });
+    });
+    await batch.commit();
+});
