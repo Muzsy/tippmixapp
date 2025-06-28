d9f23cb0eed4e555d27a32c7385ce9b8488057f9
Muzsy <150178846+Muzsy@users.noreply.github.com>
2025-06-28 22:36:26 +0200

    test: stub services for navigation tests

--- test/screens/home_appbar_title_test.dart
+++ test/screens/home_appbar_title_test.dart
@@ -3,9 +3,53 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tippmixapp/l10n/app_localizations.dart';
-import 'package:tippmixapp/screens/home_screen.dart';
+import 'dart:async';
+
+import 'package:tippmixapp/screens/home_screen.dart'
+    show
+        HomeScreen,
+        dailyBonusAvailableProvider,
+        latestBadgeProvider,
+        aiTipFutureProvider,
+        activeChallengesProvider,
+        latestFeedActivityProvider;
 import 'package:tippmixapp/screens/feed_screen.dart';
 import 'package:tippmixapp/routes/app_route.dart';
+import 'package:tippmixapp/providers/auth_provider.dart'
+    show authProvider, authServiceProvider, AuthNotifier;
+import 'package:tippmixapp/providers/stats_provider.dart';
+import 'package:tippmixapp/services/auth_service.dart';
+import 'package:tippmixapp/models/auth_state.dart';
+import 'package:tippmixapp/models/user.dart';
+
+class FakeAuthService implements AuthService {
+  final _controller = StreamController<User?>.broadcast();
+  User? _current;
+
+  @override
+  Stream<User?> authStateChanges() => _controller.stream;
+
+  @override
+  Future<User?> signInWithEmail(String email, String password) async => null;
+
+  @override
+  Future<User?> registerWithEmail(String email, String password) async => null;
+
+  @override
+  Future<void> signOut() async {
+    _current = null;
+    _controller.add(null);
+  }
+
+  @override
+  User? get currentUser => _current;
+}
+
+class FakeAuthNotifier extends AuthNotifier {
+  FakeAuthNotifier(User? user) : super(FakeAuthService()) {
+    state = AuthState(user: user);
+  }
+}
 
 void main() {
   testWidgets('AppBar shows feed title on /feed route', (tester) async {
@@ -32,6 +76,16 @@ void main() {
 
     await tester.pumpWidget(
       ProviderScope(
+        overrides: [
+          authServiceProvider.overrideWith((ref) => FakeAuthService()),
+          authProvider.overrideWith((ref) => FakeAuthNotifier(null)),
+          dailyBonusAvailableProvider.overrideWith((ref) => false),
+          userStatsProvider.overrideWith((ref) async => null),
+          aiTipFutureProvider.overrideWith((ref) async => null),
+          latestBadgeProvider.overrideWith((ref) async => null),
+          activeChallengesProvider.overrideWith((ref) => []),
+          latestFeedActivityProvider.overrideWith((ref) async => null),
+        ],
         child: MaterialApp.router(
           routerConfig: router,
           localizationsDelegates: AppLocalizations.localizationsDelegates,
@@ -69,6 +123,16 @@ void main() {
 
     await tester.pumpWidget(
       ProviderScope(
+        overrides: [
+          authServiceProvider.overrideWith((ref) => FakeAuthService()),
+          authProvider.overrideWith((ref) => FakeAuthNotifier(null)),
+          dailyBonusAvailableProvider.overrideWith((ref) => false),
+          userStatsProvider.overrideWith((ref) async => null),
+          aiTipFutureProvider.overrideWith((ref) async => null),
+          latestBadgeProvider.overrideWith((ref) async => null),
+          activeChallengesProvider.overrideWith((ref) => []),
+          latestFeedActivityProvider.overrideWith((ref) async => null),
+        ],
         child: MaterialApp.router(
           routerConfig: router,
           localizationsDelegates: AppLocalizations.localizationsDelegates,
