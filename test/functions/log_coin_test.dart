3ad95113233d665655e527aa982cf0aa8d485dbf
Muzsy <150178846+Muzsy@users.noreply.github.com>
2025-07-01 13:54:06 +0200

    Hibajavitás utáni push

--- test/functions/log_coin_test.dart
+++ test/functions/log_coin_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 // Minimal replica of the log_coin Cloud Function.
 Future<Map<String, dynamic>> callLogCoin(
-  FirebaseFirestore firestore,
+  dynamic firestore,
   Map<String, dynamic> data, {
   String? uid,
 }) async {
@@ -27,39 +27,35 @@ Future<Map<String, dynamic>> callLogCoin(
 }
 
 // Firestore fakes
-// ignore: subtype_of_sealed_class
-class FakeLogDoc extends Fake implements DocumentReference<Map<String, dynamic>> {
-  @override
+class FakeLogDoc extends Fake {
   final String id;
   final Map<String, Map<String, dynamic>> store;
   FakeLogDoc(this.id, this.store);
-  @override
+
   Future<void> set(Map<String, dynamic> data, [SetOptions? options]) async {
     store[id] = data;
   }
 }
 
-// ignore: subtype_of_sealed_class
-class FakeLogCollection extends Fake implements CollectionReference<Map<String, dynamic>> {
+class FakeLogCollection extends Fake {
   final Map<String, Map<String, dynamic>> store;
   FakeLogCollection(this.store);
-  @override
-  DocumentReference<Map<String, dynamic>> doc([String? id]) {
+
+  FakeLogDoc doc([String? id]) {
     final key = id ?? 'log_${store.length}';
     store.putIfAbsent(key, () => <String, dynamic>{});
     return FakeLogDoc(key, store);
   }
 }
 
-class FakeFirestore extends Fake implements FirebaseFirestore {
+class FakeFirestore extends Fake {
   final Map<String, Map<String, dynamic>> logs = {};
-  @override
-  CollectionReference<Map<String, dynamic>> collection(String path) {
+
+  FakeLogCollection collection(String path) {
     if (path == 'coin_logs') return FakeLogCollection(logs);
     throw UnimplementedError();
   }
 }
-
 void main() {
   test('successfully stores log', () async {
     final fs = FakeFirestore();
