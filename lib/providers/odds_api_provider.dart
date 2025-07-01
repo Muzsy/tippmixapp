eebecf191b301e5b5e621a316bf5376b1f4e38e2
Muzsy <150178846+Muzsy@users.noreply.github.com>
2025-07-01 20:29:16 +0200

    Fix analyzer warnings and update profile test

--- lib/providers/odds_api_provider.dart
+++ lib/providers/odds_api_provider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter/foundation.dart';
 
 import '../models/odds_event.dart';
 import '../models/odds_api_response.dart';
