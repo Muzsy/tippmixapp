f4b4690f74d447c0460388b6b01ca7ae031332cc
Muzsy <150178846+Muzsy@users.noreply.github.com>
2025-07-07 23:04:24 +0200

    feat(profile): add edit profile flow

---
+++ test/services/user_service_update_profile_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:tippmixapp/services/user_service.dart';
+import 'package:tippmixapp/models/user_model.dart';
+
+void main() {
+  test('updateProfile merges changes', () async {
+    final firestore = FakeFirebaseFirestore();
+    await firestore.collection('users').doc('u1').set({
+      'uid': 'u1',
+      'email': 'a@b.com',
+      'displayName': 'old',
+      'nickname': 'n',
+      'avatarUrl': '',
+      'isPrivate': false,
+      'fieldVisibility': {},
+    });
+    final service = UserService(firestore);
+    final updated = await service.updateProfile('u1', {'displayName': 'new'});
+    final doc = await firestore.collection('users').doc('u1').get();
+    expect(doc.data()?['displayName'], 'new');
+    expect(updated.displayName, 'new');
+  });
+}
