786c55b0416bc75784bbebb8ccf7747ad509a3d6
Muzsy <150178846+Muzsy@users.noreply.github.com>
2025-07-07 17:49:33 +0200

    feat(test): add firebase-free MockThemeService

--- test/mock_theme_service.dart
+++ test/mock_theme_service.dart
@@ -1,12 +1,25 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart' as fb;
+import 'package:mocktail/mocktail.dart';
 import 'package:tippmixapp/services/theme_service.dart';
 
 /// Simple mock implementation of [ThemeService] for widget tests.
 ///
 /// Returns a fixed light theme for all calls so golden tests can
 /// run without depending on shared preferences or Firebase.
+class _FakeFirebaseAuth extends Fake implements fb.FirebaseAuth {
+  @override
+  fb.User? get currentUser => null;
+}
+
 class MockThemeService extends ThemeService {
-  MockThemeService() : super();
+  MockThemeService()
+      : super(
+          prefs: null,
+          firestore: FakeFirebaseFirestore(),
+          auth: _FakeFirebaseAuth(),
+        );
 
   static const FlexScheme _defaultScheme = FlexScheme.dellGenoa;
 
