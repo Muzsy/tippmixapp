68f90dfdc628162c89fe5fb57d6905b08fb49c9a
Muzsy <150178846+Muzsy@users.noreply.github.com>
2025-07-09 05:53:10 +0200

    Implement login fixes and tests

--- packages/firebase_app_check/lib/firebase_app_check.dart
+++ packages/firebase_app_check/lib/firebase_app_check.dart
@@ -1,7 +1,12 @@
 class FirebaseAppCheck {
   FirebaseAppCheck._();
   static final FirebaseAppCheck instance = FirebaseAppCheck._();
-  Future<void> activate({AndroidProvider androidProvider = AndroidProvider.debug}) async {}
+  Future<void> activate({
+    AndroidProvider androidProvider = AndroidProvider.debug,
+    AppleProvider appleProvider = AppleProvider.debug,
+  }) async {}
 }
 
 enum AndroidProvider { debug, playIntegrity }
+
+enum AppleProvider { debug, appStore }
