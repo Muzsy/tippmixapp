d1d38d946db8c9edf67c828a6d57db07dba323a4
Muzsy <150178846+Muzsy@users.noreply.github.com>
2025-07-09 05:53:10 +0200

    Implement login fixes and tests

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -3,12 +3,17 @@ import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
+import 'firebase_options.dart';
+
 Future<void> bootstrap() async {
   WidgetsFlutterBinding.ensureInitialized();
   if (kDebugMode) {
     await FirebaseAppCheck.instance.activate(
       androidProvider: AndroidProvider.debug,
+      appleProvider: AppleProvider.debug,
     );
   }
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
