78425d92a5a246399737b5aa7028974a07823c4b
Muzsy <150178846+Muzsy@users.noreply.github.com>
2025-07-27 21:29:49 +0200

    Add registerWithEmail debug log and tests

---
+++ integration_test/register_flow_test.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tippmixapp/main.dart' as app;
+import 'package:tippmixapp/router.dart';
+
+void main() {
+  testWidgets('register flow prints log', (tester) async {
+    final logs = <String>[];
+    await runZoned(() async {
+      app.main();
+      await tester.pumpAndSettle();
+      router.go('/register');
+      await tester.pumpAndSettle();
+
+      await tester.enterText(find.byType(TextFormField).at(0), 'user@test.com');
+      await tester.enterText(find.byType(TextFormField).at(1), 'Password1!');
+      await tester.tap(find.widgetWithText(ElevatedButton, 'Continue'));
+      await tester.pumpAndSettle(const Duration(milliseconds: 400));
+
+      await tester.enterText(find.byKey(const Key('nicknameField')), 'tester');
+      await tester.tap(find.byKey(const Key('birthDateField')));
+      await tester.pumpAndSettle();
+      await tester.tap(find.text('1'));
+      await tester.tap(find.text('OK'));
+      await tester.pumpAndSettle();
+      await tester.tap(find.byType(CheckboxListTile));
+      await tester.tap(find.widgetWithText(ElevatedButton, 'Continue'));
+      await tester.pumpAndSettle(const Duration(milliseconds: 400));
+
+      await tester.tap(find.widgetWithText(ElevatedButton, 'Finish'));
+      await tester.pumpAndSettle();
+    }, zoneSpecification: ZoneSpecification(print: (_, __, ___, String msg) {
+      logs.add(msg);
+    }));
+
+    expect(logs.any((l) => l.contains('[REGISTER] registerWithEmail STARTED')), isTrue);
+  });
+}
