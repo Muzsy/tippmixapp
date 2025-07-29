5f08562c98e31301491540c18d036e57fd9c0621
Muzsy <150178846+Muzsy@users.noreply.github.com>
2025-07-29 20:11:49 +0200

    Refactor email availability check using FirebaseAuth

---
+++ test/unit/auth_repository_email_check_refactor_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:tippmixapp/services/auth_repository.dart';
+
+class _MockAuth extends Mock implements FirebaseAuth {}
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  final mockAuth = _MockAuth();
+  test('email not in use returns true', () async {
+    when(
+      () => mockAuth.fetchSignInMethodsForEmail(any()),
+    ).thenAnswer((_) async => <String>[]);
+    final repo = AuthRepository(firebaseAuth: mockAuth);
+    expect(await repo.isEmailAvailable('x@y.hu'), true);
+  });
+
+  test('email exists returns false', () async {
+    when(
+      () => mockAuth.fetchSignInMethodsForEmail(any()),
+    ).thenAnswer((_) async => <String>['password']);
+    final repo = AuthRepository(firebaseAuth: mockAuth);
+    expect(await repo.isEmailAvailable('x@y.hu'), false);
+  });
+}
