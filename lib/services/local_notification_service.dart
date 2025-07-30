bce5756616e937af84deabb50330d2e91d8d6f2c
Muzsy <150178846+Muzsy@users.noreply.github.com>
2025-07-30 22:20:48 +0200

    chore(cleanup): remove TODO files and debug prints

--- lib/services/local_notification_service.dart
+++ lib/services/local_notification_service.dart
@@ -2,6 +2,7 @@ class LocalNotificationService {
   const LocalNotificationService();
 
   Future<void> scheduleStreakWarning() async {
-    // TODO: implement local notification scheduling once plugin is available.
+    // Notification scheduling is not yet implemented.
+    return;
   }
 }
