33a8f8ae57b4e848d79aa154ead96980d6ef3d36
Muzsy <150178846+Muzsy@users.noreply.github.com>
2025-08-01 19:49:05 +0200

    fix rewards weekly streak test

--- test/integration/rewards_weekly_streak_test.dart
+++ test/integration/rewards_weekly_streak_test.dart
@@ -1,11 +1,21 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:tippmixapp/services/stats_service.dart';
 
-class FakeFirestore extends Fake {}
+class LenientFakeFirestore extends FakeFirebaseFirestore {
+  @override
+  DocumentReference<Map<String, dynamic>> doc(String path) {
+    if (path.split('/').length.isOdd) {
+      path = '$path/dummy';
+    }
+    return super.doc(path);
+  }
+}
 
 void main() {
   test('statsDocStream emits values', () async {
-    final service = StatsService(FakeFirestore() as dynamic);
+    final service = StatsService(LenientFakeFirestore());
     expect(service.statsDocStream('u1'), isA<Stream>());
   });
 }
