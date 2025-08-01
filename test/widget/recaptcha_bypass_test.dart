2b2907a0358890f5e8ef7dc95304b53c45960e27
Muzsy <150178846+Muzsy@users.noreply.github.com>
2025-08-01 19:59:05 +0200

    Fix recaptcha bypass widget test

--- test/widget/recaptcha_bypass_test.dart
+++ test/widget/recaptcha_bypass_test.dart
@@ -3,12 +3,56 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tippmixapp/screens/register_wizard.dart';
 import 'package:tippmixapp/l10n/app_localizations.dart';
+import 'package:tippmixapp/providers/hibp_service_provider.dart';
+import 'package:tippmixapp/providers/recaptcha_service_provider.dart';
+import 'package:tippmixapp/providers/auth_repository_provider.dart';
+import 'package:tippmixapp/providers/auth_provider.dart';
+import 'package:tippmixapp/services/hibp_service.dart';
+import 'package:tippmixapp/services/recaptcha_service.dart';
+import 'package:tippmixapp/services/auth_repository.dart';
+import 'package:tippmixapp/services/analytics_service.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import '../mocks/mock_auth_service.dart';
+
+class FakeHIBPService extends HIBPService {
+  FakeHIBPService() : super();
+
+  @override
+  Future<bool> isPasswordPwned(String password) async => false;
+}
+
+class FakeRecaptchaService extends RecaptchaService {
+  FakeRecaptchaService() : super(secret: 's');
+
+  @override
+  Future<bool> verifyToken(String token) async => true;
+}
+
+class FakeAuthRepository implements AuthRepository {
+  @override
+  Future<bool> isEmailAvailable(String email) async => true;
+}
+
+class FakeFirebaseAnalytics extends Fake implements FirebaseAnalytics {}
+
+class _FakeAnalyticsService extends AnalyticsService {
+  _FakeAnalyticsService() : super(FakeFirebaseAnalytics());
+}
 
 void main() {
   testWidgets('recaptcha bypass allows navigation to step2', (tester) async {
     await tester.pumpWidget(
-      const ProviderScope(
-        child: MaterialApp(
+      ProviderScope(
+        overrides: [
+          hibpServiceProvider.overrideWith((ref) => FakeHIBPService()),
+          recaptchaServiceProvider.overrideWith((ref) => FakeRecaptchaService()),
+          authServiceProvider.overrideWith((ref) => MockAuthService()),
+          authRepositoryProvider.overrideWith((ref) => FakeAuthRepository()),
+          analyticsServiceProvider.overrideWith(
+            (ref) => _FakeAnalyticsService(),
+          ),
+        ],
+        child: const MaterialApp(
           localizationsDelegates: AppLocalizations.localizationsDelegates,
           supportedLocales: AppLocalizations.supportedLocales,
           locale: Locale('en'),
@@ -18,10 +62,10 @@ void main() {
     );
 
     await tester.enterText(find.byType(TextFormField).first, 'test@exam.com');
-    await tester.enterText(find.byType(TextFormField).at(1), 'Teszt123!');
+    await tester.enterText(find.byType(TextFormField).at(1), 'TestPassword1!');
     await tester.pump(const Duration(milliseconds: 350));
     await tester.tap(find.widgetWithText(ElevatedButton, 'Continue'));
-    await tester.pumpAndSettle();
+    await tester.pumpAndSettle(const Duration(seconds: 1));
 
     expect(find.byKey(const Key('nicknameField')), findsOneWidget);
   });
