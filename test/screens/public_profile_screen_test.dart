78344cde0f074ad1f13c09be37049f2fdd7c01a9
Muzsy <150178846+Muzsy@users.noreply.github.com>
2025-08-01 23:20:42 +0200

    Fix PublicProfileScreen test

--- test/screens/public_profile_screen_test.dart
+++ test/screens/public_profile_screen_test.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'dart:typed_data';
 import 'package:tippmixapp/l10n/app_localizations.dart';
 import 'package:tippmixapp/l10n/app_localizations_en.dart';
 import 'package:tippmixapp/models/user_model.dart';
 import 'package:tippmixapp/screens/public_profile_screen.dart';
+import 'package:tippmixapp/constants.dart';
 
 Widget _wrap(Widget child) {
   return MaterialApp(
@@ -14,14 +18,118 @@ Widget _wrap(Widget child) {
   );
 }
 
+void _mockAssets() {
+  TestWidgetsFlutterBinding.ensureInitialized().defaultBinaryMessenger
+      .setMockMessageHandler('flutter/assets', (ByteData? message) async {
+    final key = utf8.decode(message!.buffer.asUint8List());
+    if (key == 'AssetManifest.bin') {
+      final manifest = <String, Object?>{
+        kDefaultAvatarPath: const [
+          <String, Object?>{'asset': kDefaultAvatarPath},
+        ],
+      };
+      return const StandardMessageCodec().encodeMessage(manifest)!;
+    }
+    if (key == 'AssetManifest.json') {
+      final manifest = json.encode({
+        kDefaultAvatarPath: const [
+          <String, String>{'asset': kDefaultAvatarPath},
+        ],
+      });
+      return ByteData.view(
+        Uint8List.fromList(utf8.encode(manifest)).buffer,
+      );
+    }
+    if (key == kDefaultAvatarPath) {
+      return ByteData.view(Uint8List.fromList(_kTransparentImage).buffer);
+    }
+    return null;
+  });
+}
+
+void _resetAssets() {
+  TestWidgetsFlutterBinding.ensureInitialized().defaultBinaryMessenger
+      .setMockMessageHandler('flutter/assets', null);
+}
+
+const List<int> _kTransparentImage = <int>[
+  0x89,
+  0x50,
+  0x4E,
+  0x47,
+  0x0D,
+  0x0A,
+  0x1A,
+  0x0A,
+  0x00,
+  0x00,
+  0x00,
+  0x0D,
+  0x49,
+  0x48,
+  0x44,
+  0x52,
+  0x00,
+  0x00,
+  0x00,
+  0x01,
+  0x00,
+  0x00,
+  0x00,
+  0x01,
+  0x08,
+  0x06,
+  0x00,
+  0x00,
+  0x00,
+  0x1F,
+  0x15,
+  0xC4,
+  0x89,
+  0x00,
+  0x00,
+  0x00,
+  0x0A,
+  0x49,
+  0x44,
+  0x41,
+  0x54,
+  0x78,
+  0x9C,
+  0x63,
+  0x00,
+  0x01,
+  0x00,
+  0x00,
+  0x05,
+  0x00,
+  0x01,
+  0x0D,
+  0x0A,
+  0x2D,
+  0xB4,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x49,
+  0x45,
+  0x4E,
+  0x44,
+  0xAE,
+];
+
 void main() {
+  setUp(_mockAssets);
+  tearDown(_resetAssets);
+
   testWidgets('private profile shows only nickname', (tester) async {
     final user = UserModel(
       uid: 'u1',
       email: 'a@b.com',
       displayName: 'Disp',
       nickname: 'Nick',
-      avatarUrl: '',
+      avatarUrl: kDefaultAvatarPath,
       isPrivate: true,
       fieldVisibility: const {'city': true, 'country': true},
     );
@@ -38,7 +146,7 @@ void main() {
       email: 'a@b.com',
       displayName: 'Disp',
       nickname: 'Nick',
-      avatarUrl: '',
+      avatarUrl: kDefaultAvatarPath,
       isPrivate: false,
       fieldVisibility: const {'city': true, 'country': false},
     );
