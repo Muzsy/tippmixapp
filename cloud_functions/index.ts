c3ffea539c1c3cbd13846e7a0ecce35ee764aaad
Muszy <akos.muller@gmail.com>
2025-08-10 01:12:22 +0200

    index.ts

--- cloud_functions/index.ts
+++ cloud_functions/index.ts
@@ -1,5 +1,12 @@
-export { onUserCreate, coin_trx } from './coin_trx.logic';
-export { log_coin } from './log_coin';
-export { onFriendRequestAccepted } from './friend_request';
-export const match_finalizer = async (message: any, context: any) => { /* ... */ };
+ import * as functions from 'firebase-functions';
+ import { match_finalizer as matchFinalizerHandler } from './src/match_finalizer';
 
+ export { onUserCreate, coin_trx } from './coin_trx.logic';
+ export { log_coin } from './log_coin';
+ export { onFriendRequestAccepted } from './friend_request';
+
+ // Pub/Sub trigger: 'result-check' topic, régió: europe-central2
+ export const match_finalizer = functions
+   .region('europe-central2')
+   .pubsub.topic('result-check')
+   .onPublish(matchFinalizerHandler);
